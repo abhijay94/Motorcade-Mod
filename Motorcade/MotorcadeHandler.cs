65fe53daeacfb8071fe6393b218106a455a8f0da
Abhijay Kumar <abhijaynsit@gmail.com>
2019-05-21 23:29:49 +0530

    Changed the follow method to drive behind fixed pos

--- Motorcade/MotorcadeHandler.cs
+++ Motorcade/MotorcadeHandler.cs
@@ -10,37 +10,35 @@ public static class EntryPoint
         Ped[] drivers = new Ped[6];
         Vehicle[] motorcadeVehicles = new Vehicle[6];
 
-        bool allSetUp = setUpPedsAndVehicles(ref drivers, ref motorcadeVehicles);
+        bool allSetUp = setUpPedsAndVehicles(drivers, motorcadeVehicles);
 
         if (allSetUp)
         {
             // Makes the first vehilce tail the player
-            followTheVehice(motorcadeVehicles[0], Game.LocalPlayer.Character.CurrentVehicle);
+            //followTheVehice(motorcadeVehicles[0], Game.LocalPlayer.Character.CurrentVehicle);
 
             // Do the same for all remaining vehicles. All vehicles tail their predecessor.
-            for (int i = 1; i < 6; i++)
+            for (int i = 0; i < 6; i++)
             {
-                followTheVehice(motorcadeVehicles[i - 1], motorcadeVehicles[i]);
+                followTheLeader(motorcadeVehicles[i], 2f + i*12f);
             }
         }
         else
         {
             Game.DisplayNotification("Setting up vehicles and peds failed.");
         }
-        while (true)
-        {
-            if (Albo1125.Common.CommonLibrary.ExtensionMethods.IsKeyDownRightNowComputerCheck(PropertiesInitializer.motorcadeModifierKey) &&
-                Albo1125.Common.CommonLibrary.ExtensionMethods.IsKeyDownRightNowComputerCheck(PropertiesInitializer.motorcadeEndKey))
-            {
-                doCleanUp(ref motorcadeVehicles, ref drivers);
-                Game.DisplayNotification("Motorcade is cleaned up!");
-                break;
-            }
-        }
-
+        //while (true)
+        //{
+        //    if (Albo1125.Common.CommonLibrary.ExtensionMethods.IsKeyDownRightNowComputerCheck(PropertiesInitializer.motorcadeModifierKey) &&
+        //        Albo1125.Common.CommonLibrary.ExtensionMethods.IsKeyDownRightNowComputerCheck(PropertiesInitializer.motorcadeEndKey))
+        //    {
+        //        doCleanUp(motorcadeVehicles, drivers);
+        //        Game.DisplayNotification("Motorcade is cleaned up!");
+        //    }
+        //}
     }
 
-    private static void doCleanUp(ref Vehicle[] motorcadeVehicles, ref Ped[] drivers)
+    private static void doCleanUp(Vehicle[] motorcadeVehicles, Ped[] drivers)
     {
         for (int i = 0; i < 6; i++)
         {
@@ -51,10 +49,10 @@ public static class EntryPoint
         }
     }
 
-    public static bool setUpPedsAndVehicles(ref Ped[] drivers, ref Vehicle[] motorcadeVehicles)
+    public static bool setUpPedsAndVehicles(Ped[] drivers, Vehicle[] motorcadeVehicles)
     {
-        float[] vehicleSpanPositionConsts = { 20f, 35f, 50f, 70f, 85f, 100f };
-        string[] vehicleModels = { "FBI2", "FBI2", "STRETCH", "FBI", "FBI", "RIOT" };
+        float[] vehicleSpanPositionConsts = { -20f, -35f, -50f, -70f, -85f, -100f };
+        string[] vehicleModels = { "WASHINGTON", "WASHINGTON", "FBI2", "EMPEROR", "FBI2", "FBI2" };
 
         // Get a position 15 meters in front of the player.
         Vector3[] vehicleSpanPositions = new Vector3[6];// Game.LocalPlayer.Character.GetOffsetPositionFront(20f);
@@ -64,13 +62,13 @@ public static class EntryPoint
         for (int i = 0; i < 6; i++)
         {
             vehicleSpanPositions[i] = Game.LocalPlayer.Character.GetOffsetPositionFront(vehicleSpanPositionConsts[i]);
-            driverSpanPositions[i] = Game.LocalPlayer.Character.GetOffsetPositionFront(vehicleSpanPositionConsts[i] - 5f);
+            driverSpanPositions[i] = Game.LocalPlayer.Character.GetOffsetPositionFront(vehicleSpanPositionConsts[i] + 3f);
         }
 
         // Spawn driver peds
         for (int i = 0; i < 6; i++)
         {
-            drivers[i] = new Ped("cs_fbisuit_01", driverSpanPositions[i], 130f);
+            drivers[i] = new Ped("cs_fbisuit_01", driverSpanPositions[i], 310f);
             // Prevent the character from doing something we haven't told it to do (Eg. fleeing from gunfire).
             drivers[i].BlockPermanentEvents = true;
         }
@@ -81,7 +79,7 @@ public static class EntryPoint
         // Spawn Vehicles
         for (int i = 0; i < 6; i++)
         {
-            motorcadeVehicles[i] = new Vehicle(vehicleModels[i], vehicleSpanPositions[i], 130f);
+            motorcadeVehicles[i] = new Vehicle(vehicleModels[i], vehicleSpanPositions[i], 310f);
         }
 
         // Wait for 3 seconds   
@@ -123,7 +121,7 @@ public static class EntryPoint
             // Are all characters in the vehicle?
             if (drivers[0].IsInVehicle(motorcadeVehicles[0], false) && drivers[1].IsInVehicle(motorcadeVehicles[1], false)
                 && drivers[2].IsInVehicle(motorcadeVehicles[2], false) && drivers[3].IsInVehicle(motorcadeVehicles[3], false)
-                && drivers[4].IsInVehicle(motorcadeVehicles[4], false))
+                && drivers[4].IsInVehicle(motorcadeVehicles[4], false) && drivers[5].IsInVehicle(motorcadeVehicles[5], false))
             {
                 // Stop waiting. Call execution success.
                 return true;
@@ -133,7 +131,7 @@ public static class EntryPoint
         }
     }
 
-    public static void followTheVehice(Vehicle following, Vehicle followed)
+    public static void followTheLeader(Vehicle following, float distance)
     {
         isMotorcadeInProgress = true;
         GameFiber.StartNew(delegate
@@ -142,11 +140,13 @@ public static class EntryPoint
             {
                 following.ShouldVehiclesYieldToThisVehicle = true;
                 following.IsSirenOn = true;
-                followed.ShouldVehiclesYieldToThisVehicle = true;
-                followed.IsSirenOn = true;
+                //followed.ShouldVehiclesYieldToThisVehicle = true;
+                //followed.IsSirenOn = true;
 
                 Game.LogTrivial("Motorcade starting...");
                 Ped playerPed = Game.LocalPlayer.Character;
+
+                Vehicle leader = playerPed.CurrentVehicle;
                 if (!playerPed.IsInAnyVehicle(false))
                 {
                     isMotorcadeInProgress = false;
@@ -165,13 +165,13 @@ public static class EntryPoint
                 Blip blip = followingDriver.AttachBlip();
                 blip.Flash(500, -1);
                 blip.Color = System.Drawing.Color.Aqua;
-                followingDriver.Tasks.DriveToPosition(followed.GetOffsetPosition(Vector3.RelativeBack * 3f), 9f, VehicleDrivingFlags.FollowTraffic | VehicleDrivingFlags.YieldToCrossingPedestrians);
-                GameFiber.Sleep(100);
+                followingDriver.Tasks.DriveToPosition(leader.GetOffsetPosition(Vector3.RelativeBack * distance), 9f, VehicleDrivingFlags.FollowTraffic | VehicleDrivingFlags.YieldToCrossingPedestrians);
+                //GameFiber.Sleep(100);
                 float speed = 13f;
                 while (true)
                 {
 
-                    followingDriver.Tasks.DriveToPosition(followed.GetOffsetPosition(Vector3.RelativeBack * 3f), speed, VehicleDrivingFlags.IgnorePathFinding);
+                    followingDriver.Tasks.DriveToPosition(leader.GetOffsetPosition(Vector3.RelativeBack * distance), speed, VehicleDrivingFlags.IgnorePathFinding);
                     GameFiber.Sleep(60);
 
                     if (!isMotorcadeInProgress)
@@ -179,15 +179,15 @@ public static class EntryPoint
                         break;
                     }
                     // Break if any one of the drivers gets down from their vehicle
-                    if (!playerPed.IsInVehicle(followed, false))
+                    if (!playerPed.IsInVehicle(leader, false))
                     {
                         break;
                     }
-                    speed = followed.Speed;
-                    if (Vector3.Distance(followed.Position, following.Position) > 150f)
+                    speed = leader.Speed;
+                    if (Vector3.Distance(leader.Position, following.Position) > 500f)
                     {
-                        following.Position = followed.GetOffsetPosition(Vector3.RelativeBack * 7f);
-                        following.Heading = followed.Heading;
+                        following.Position = leader.GetOffsetPosition(Vector3.RelativeBack * distance);
+                        following.Heading = leader.Heading;
                         blip.Delete();
                         blip = followingDriver.AttachBlip();
                         blip.Flash(500, -1);
@@ -200,7 +200,7 @@ public static class EntryPoint
                         {
                             speed = 10f;
                         }
-                        if (Vector3.Distance(followed.Position, following.Position) > 21f)
+                        if (Vector3.Distance(leader.Position, following.Position) > 21f)
                         {
                             speed = 17f;
                         }
@@ -213,6 +213,8 @@ public static class EntryPoint
             }
             finally
             {
+                following.Driver.Delete();
+                following.Delete();
                 isMotorcadeInProgress = false;
             }
         });
